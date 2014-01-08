4a816f6466a1d3ce992787f7633803c4c808294b
Ryan Senior <ryan.senior@puppetlabs.com>
2014-01-08 17:03:11 -0600

    Changed the group id to puppetlabs, released as 0.2.0-pre-1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "0.2.0-SNAPSHOT"
+(defproject puppetlabs/schema "0.2.0-pre-1"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/prismatic/schema"
   :license {:name "Eclipse Public License"
